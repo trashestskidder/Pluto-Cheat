5a7be47951b0e29d857d9e3f5ed4227f7abf0109
trashestskidder <itzmexsync@gmail.com>
2024-11-20 02:39:11 +0700

    Update metadata-main.lua

--- client/metatable/metadata-main.lua
+++ client/metatable/metadata-main.lua
@@ -1,7 +1,8 @@
-local GuideModule = require(game.ReplicatedStorage:WaitForChild("GuideModule")).ChangeDisplayedNPC
-local Death = require(game.ReplicatedStorage.Effect.Container.Death)
-local Respawn = require(game.ReplicatedStorage.Effect.Container.Respawn)
-
-hookfunction(GuideModule, function() return false end)
-hookfunction(Death, function() return false end)
-hookfunction(Respawn, function() return false end)
+local metadata = {
+  hub = {
+    name = "Pluto Cheat",
+    game = "Blox Fruits",
+    version = "1.0",
+  },
+}
+return metadata
